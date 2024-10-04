3ef88a2e1167cd95dff2be0dafd23b59ce4bda60
mickberryz <sa.thawatchai@gmail.com>
2024-10-04 13:59:27 +0100

    fix discount regularPrice on ListingItem

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -27,10 +27,10 @@ export default function ListingItem({ listing }) {
             {listing.description}
           </p>
           <p className="text-slate-500 mt-2 font-semibold flex items-center">
-            $
-            {listing.offer
+            $ {+listing.regularPrice - +listing.discountPrice}
+            {/* {listing.offer
               ? listing.discountPrice.toLocaleString("en-US")
-              : listing.regularPrice.toLocaleString("en-US")}
+              : listing.regularPrice.toLocaleString("en-US")} */}
             {listing.type === "rent" && " / month"}
           </p>
           <div className="text-slate-700 flex gap-4">
